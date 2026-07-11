338f20f773ceb89c7ee9eec2482d7c5088442c90
Apoorv <apoorvdeshmukh3@gmail.com>
2026-07-11 15:00:44 +0530

    fixed various errors

--- users/models.py
+++ users/models.py
@@ -8,9 +8,9 @@ class Profile(models.Model):
     image=models.ImageField(default='default.jpg',upload_to='profile_pics')
     def __str__(self):
         return f'{self.user.username} Profile'
-    def save(self):
-        super().save()
-        img=Image.open(self.open.path)
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+        img=Image.open(self.image.path)
         if img.height>300 or img.width >300:
             output_size=(300, 300)
             img.thumbnail(output_size)
