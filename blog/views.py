0aec253bcb3414e9a824120e949f57b80ae2ab9f
Apoorv <apoorvdeshmukh3@gmail.com>
2026-07-13 23:29:30 +0530

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import ( LoginRequiredMixin,
                                         UserPassesTestMixin
 )
-
+from django.contrib.auth.models import User
 from django.views.generic import ( ListView, 
                                   DetailView,
                                  CreateView,
@@ -25,8 +25,18 @@ class PostListView(ListView):
     templat_name='blog/home.html'
     context_object_name='posts'
     ordering=['-date_posted']
+    paginate_by=5
 
+class UserPostListView(ListView):
+    model=Post
+    templat_name='blog/user_posts.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
+    paginate_by=5
 
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 class PostDetailView(DetailView):
     model=Post
     
@@ -36,6 +46,7 @@ class PostCreateView(LoginRequiredMixin,CreateView):
     def form_valid(self,form):
         form.instance.author=self.request.user
         return super().form_valid(form)
+
 class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model=Post
     fields=['title','content']
@@ -56,5 +67,6 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,DeleteView):
         if self.request.user==post.author:
             return True
         return False
+
 def about(request):
     return render(request,'blog/about.html',{'title': "About"})
