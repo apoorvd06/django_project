34fe309cd1a94976c964f2cc1f9cccfbdd23d7b2
Apoorv <apoorvdeshmukh3@gmail.com>
2026-07-14 22:43:38 +0530

    added pasowrd reset route

--- InkOrbit/urls.py
+++ InkOrbit/urls.py
@@ -27,6 +27,15 @@ urlpatterns = [
     path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), 
+         name='Password_reset'),
+    path('password-reset/done',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+         name='Password_reset-Done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+         name='Password_reset-confirm'),
     path('', include('blog.urls')),
 ]
 if settings.DEBUG:
